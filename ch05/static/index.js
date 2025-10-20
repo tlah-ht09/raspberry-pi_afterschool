b6f9a7135b964cf397cca66e5bf92f71dceb81fc
정현태 <jht3021@gmail.com>
2025-10-20 20:32:59 +0900

    경로 수정

--- ch05/static/index.js
+++ ch05/static/index.js
@@ -5,7 +5,8 @@ const go = 10;
 
 window.addEventListener("keydown", async (e) => {
   if (e.key == "ArrowLeft") {
-    img.src = "./천칭 왼쪽 복사본.png";
+    img.src =
+      "/Users/mac/Desktop/ch_/raspberry-pi_afterschool/ch05/static/천칭 왼쪽 복사본.png";
     console.log("left");
     const response = await fetch(`/send`, {
       method: "POST",
@@ -13,7 +14,8 @@ window.addEventListener("keydown", async (e) => {
       body: JSON.stringify(go),
     });
   } else if (e.key == "ArrowRight") {
-    img.src = "./천칭 오른쪽 복사본.png";
+    img.src =
+      "/Users/mac/Desktop/ch_/raspberry-pi_afterschool/ch05/static/천칭 오른쪽 복사본.png";
     console.log("right");
     const response = await fetch(`/send`, {
       method: "POST",
