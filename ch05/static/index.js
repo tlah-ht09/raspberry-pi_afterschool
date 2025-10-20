a9d53a0eb5721bbd6712744bbef711bf7df7d77c
정현태 <jht3021@gmail.com>
2025-10-20 19:58:54 +0900

    방향 수정

--- ch05/static/index.js
+++ ch05/static/index.js
@@ -4,14 +4,14 @@ const send = document.getElementById("send");
 const go = 5;
 
 window.addEventListener("keydown", async (e) => {
-  if (e.key == LeftArrow) {
+  if (e.key == "ArrowLeft") {
     console.log("left");
     const response = await fetch(`/send`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(go),
     });
-  } else if (e.key == RightArrow) {
+  } else if (e.key == "ArrowRight") {
     console.log("right");
     const response = await fetch(`/send`, {
       method: "POST",
