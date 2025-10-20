bee303017a78384c6b122310b3c3400122b6b7f2
정현태 <jht3021@gmail.com>
2025-10-20 19:43:50 +0900

    test

--- ch05/servo.py
+++ ch05/servo.py
@@ -4,5 +4,8 @@ conn = pymysql.connect(host='localhost', user='root', password='q1w2e3', db='stu
 cur = conn.cursor() #SQL 문을 실행하거나 실행된 결과를 돌려받는 통로
 
 def add_status(status):
-    cur.execute("insert into record_angle(status) values('{0}')".format(status))
-    conn.commit()
+    try:
+        cur.execute("insert into record_angle(status) values('{0}')".format(status))
+        conn.commit()
+    except:
+        print('f')
