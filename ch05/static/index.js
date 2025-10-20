a06b9dc7d05f0b3cefd326d42bb4f81d460a0624
정현태 <jht3021@gmail.com>
2025-10-20 19:56:18 +0900

    기능 변경

--- ch05/static/index.js
+++ ch05/static/index.js
@@ -1,10 +1,20 @@
 const input = document.getElementById("input");
 const send = document.getElementById("send");
 
-send.addEventListener("click", async () => {
-  const response = await fetch(`/send`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(input.value),
-  });
+const go = 5;
+
+window.addEventListener("keydown", async (e) => {
+  if (e.key == LeftArrow) {
+    const response = await fetch(`/send`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(go),
+    });
+  } else if (e.key == RightArrow) {
+    const response = await fetch(`/send`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(-go),
+    });
+  }
 });
