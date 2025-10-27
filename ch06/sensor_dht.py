75e426e6c8622985c30ba989aeac8663dfaf334a
정현태 <jht3021@gmail.com>
2025-10-27 18:09:38 +0900

    python 코드 오류 메시지 수정

--- ch06/sensor_dht.py
+++ ch06/sensor_dht.py
@@ -2,9 +2,9 @@ import adafruit_dht
 import board
 
 def get_now():
-    try :
-        sensor = adafruit_dht.DHT11(board.D18)
-        temperature = sensor.temperature
-        huminity = sensor.humidity
-        return [temperature, huminity]
-    except : return '실패'
+
+    sensor = adafruit_dht.DHT11(board.D18)
+    temperature = sensor.temperature
+    huminity = sensor.humidity
+    return [temperature, huminity]
+    
