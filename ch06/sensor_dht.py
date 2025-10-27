7fb463a9f45a61fdda5c4742236d0fe6136a835e
정현태 <jht3021@gmail.com>
2025-10-27 19:08:15 +0900

    ㅠㅠ

--- ch06/sensor_dht.py
+++ ch06/sensor_dht.py
@@ -4,12 +4,10 @@ import board
 sensor = adafruit_dht.DHT11(board.D14)
 
 def get_now():
-    try:
-        temperature = sensor.temperature
-        huminity = sensor.humidity
-        return [temperature, huminity]
+
+    temperature = sensor.temperature
+    huminity = sensor.humidity
+    return [temperature, huminity]
     
-    except : 
-        print("실패")
-        return '실패'
+
 
