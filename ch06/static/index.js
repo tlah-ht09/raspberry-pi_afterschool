94bb042b42c17946a1959810ad7736d68e316168
정현태 <jht3021@gmail.com>
2025-10-27 17:47:25 +0900

    온습도 테스트

---
+++ ch06/static/index.js
@@ -0,0 +1,16 @@
+const now = document.getElementById("now");
+const record = document.getElementById("record");
+
+now.addEventListener("click", async () => {
+  const response = await fetch("/now", {
+    method: "GET",
+  });
+  console.log(response);
+});
+
+record.addEventListener("click", async () => {
+  const response = await fetch("/now", {
+    method: "GET",
+  });
+  console.log(response);
+});
