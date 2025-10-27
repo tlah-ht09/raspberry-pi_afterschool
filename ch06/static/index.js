d4d8e23e7f610dd1307fc03f20e1c1cab9956cc1
정현태 <jht3021@gmail.com>
2025-10-27 19:02:02 +0900

    js 변경

--- ch06/static/index.js
+++ ch06/static/index.js
@@ -5,7 +5,8 @@ now.addEventListener("click", async () => {
   const response = await fetch("/now", {
     method: "GET",
   });
-  console.log(response.body);
+  const data = await response.json(); // JSON으로 파싱
+  console.log("온도:", data.temperature, "습도:", data.humidity);
 });
 
 record.addEventListener("click", async () => {
