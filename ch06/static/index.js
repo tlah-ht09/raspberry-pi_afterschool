735ba5d709bb90143042914b1ebd184a3ee4a5d4
정현태 <jht3021@gmail.com>
2025-10-27 19:24:49 +0900

    js로 화면에 표 출력

--- ch06/static/index.js
+++ ch06/static/index.js
@@ -1,5 +1,6 @@
 const now = document.getElementById("now");
 const record = document.getElementById("record");
+const table = document.getElementById("table");
 
 now.addEventListener("click", async () => {
   const response = await fetch("/now", {
@@ -7,6 +8,17 @@ now.addEventListener("click", async () => {
   });
   const data = await response.json(); // JSON으로 파싱
   console.log("온도:", data[0], "습도:", data[1]);
+  const newRow = table.insertRow(-1);
+
+  const cellNo = newRow.insertCell(0);
+  const cellDate = newRow.insertCell(1);
+  const cellTemp = newRow.insertCell(2);
+  const cellHum = newRow.insertCell(3);
+
+  cellNo.innerHTML = null;
+  cellDate.innerHTML = null;
+  cellTemp.innerHTML = data[0];
+  cellHum.innerHTML = data[1];
 });
 
 record.addEventListener("click", async () => {
