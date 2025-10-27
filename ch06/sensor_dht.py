da223238d978f8bf098f752da20e96a7b36c7367
heidtyj <hjsj23430@naver.com>
2025-10-27 09:50:27 +0000

    500에러 수정

--- ch06/sensor_dht.py
+++ ch06/sensor_dht.py
@@ -1,9 +1,9 @@
 import adafruit_dht
 import board
 
-def get_now():
+sensor = adafruit_dht.DHT11(board.D18)
 
-    sensor = adafruit_dht.DHT11(board.D18)
+def get_now():
     temperature = sensor.temperature
     huminity = sensor.humidity
     return [temperature, huminity]
